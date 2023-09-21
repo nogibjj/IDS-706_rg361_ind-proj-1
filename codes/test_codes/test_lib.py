bba54cbd28796b8bc25bdf8084874bc65356ec22
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2023-09-21 16:29:05 +0000

    updated test_lib.py

--- codes/test_codes/test_lib.py
+++ codes/test_codes/test_lib.py
@@ -6,7 +6,7 @@ import polars as pl
 import sys
 
 sys.path.insert(0, "./codes/project_codes")
-from lib import select_col  # noqa: E402
+from lib import select_col, summary_stats  # noqa: E402
 
 
 def test_select_col():
@@ -29,4 +29,19 @@ def test_select_col():
     assert -1 == select_col(df, 3)
 
 
+def test_summary_stats():
+    """Test the summary_stats function from lib file"""
+
+    # Create the polars DataFrame from the sample file
+    fname = "./resources/blood_pressure.csv"
+    df = pl.read_csv(fname)
+    col_name = "bp_before"
+    assert [
+        df[col_name].mean(),
+        df[col_name].median(),
+        df[col_name].std(),
+    ] == summary_stats(df, col_name)
+
+
 test_select_col()
+test_summary_stats()
